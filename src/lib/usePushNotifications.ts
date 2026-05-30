9a4aa6971777a57e315fc1a331cf64d875ad35ea
sarabjobtradeos <sarabjobtradeos@gmail.com>
2026-05-30 20:30:50 +0530

    Update usePushNotifications.ts

--- src/lib/usePushNotifications.ts
+++ src/lib/usePushNotifications.ts
@@ -96,7 +96,7 @@ export function usePushNotifications() {
 }
 
 // Convert VAPID key from base64url to Uint8Array
-function urlBase64ToUint8Array(base64String: string): Uint8Array {
+function urlBase64ToUint8Array(base64String: string): ArrayBuffer {
   const padding = '='.repeat((4 - (base64String.length % 4)) % 4)
   const base64 = (base64String + padding).replace(/-/g, '+').replace(/_/g, '/')
   const rawData = window.atob(base64)
@@ -104,5 +104,5 @@ function urlBase64ToUint8Array(base64String: string): Uint8Array {
   for (let i = 0; i < rawData.length; ++i) {
     outputArray[i] = rawData.charCodeAt(i)
   }
-  return outputArray
+  return outputArray.buffer
 }
